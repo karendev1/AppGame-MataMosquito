eb65b7604e58b063cecd0b11526c3c7dfc43ca85
karen <karen@outlab.rio>
2020-12-26 19:20:41 -0300

    Criando posições randômicas

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,23 @@ function ajustaTamanhoPalcoJogo(){
 }
 ajustaTamanhoPalcoJogo()
 
+//Criando posições randômicas
 
+var posicaoX = Math.floor(Math.random() * width) - 90
+var posicaoY = Math.floor(Math.random() * height) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+console.log(posicaoX, posicaoY)
+
+//Criar o elemento html
+
+var mosquito = document.createElement('img')
+mosquito.src = 'assets/imagens/mosca.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
