8ff8b622ca491a10faa3e1d52a0d412a907dffa0
karen <karen@outlab.rio>
2020-12-26 18:54:49 -0300

    Definindo a dimensão do palco do jogo

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//Encontrando altura e largura da página
+
+var width;
+var height;
+
+function ajustaTamanhoPalcoJogo(){
+     width = window.innerWidth
+     height = window.innerHeight
+
+     console.log(width, height)
+}
+ajustaTamanhoPalcoJogo()
+
+
